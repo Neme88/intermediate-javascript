6d4294edc898e65baaf52a9b1fbc7166db22025f
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-06-14 15:06:40 +0200

    added a function that accept array and return the largest number inside the array

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-function enterYourNumber(){
+/*function enterYourNumber(){
     let userInput = prompt("Enter a number? ")
     return userInput;
 
@@ -12,3 +12,24 @@ function checkIfEvenOrOdd(num){
     }
 };
 checkIfEvenOrOdd(userInput);
+*/
+
+function findLargestNumber(array) {
+    if (array.length === 0) {
+        throw new Error("Empty Array");
+    }
+
+    let largest = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
+        }
+    }
+    return largest;
+}
+
+// Example usage
+const numbers = [10, 5, 100, 19, 35, 99];
+const largestNumber = findLargestNumber(numbers);
+console.log(largestNumber); // Outputs is 100
+
