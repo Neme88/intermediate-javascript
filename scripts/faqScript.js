5b2ff5c3f555a38f968958b79c8196a5363deeb7
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-06-22 18:31:07 +0200

    added event listener to the submit button

--- scripts/faqScript.js
+++ scripts/faqScript.js
@@ -1,6 +1,6 @@
 const fraqQuestions = document.querySelectorAll('.question');
 fraqQuestions.forEach( function(element) {
-    
+
     element.addEventListener('click', function () {
         fraqQuestions.forEach( function(removeActive) {
             removeActive.classList.remove('active');
@@ -10,3 +10,34 @@ fraqQuestions.forEach( function(element) {
     });
 
 });
+window.addEventListener('DOMContentLoaded', function() {
+    const newFaq = document.getElementById('newFaq');
+    const textarea = document.getElementById('message');
+    const feedback = document.getElementById('feedback');
+
+    newFaq.addEventListener('submit', function (event) {
+        event.preventDefault();
+        checkTextarea();
+    });
+
+        
+        function checkTextarea() {
+            if (textarea.value.trim() === '') {
+                feedback.textContent = "Empty textarea.";
+                feedback.className = "Message error";
+            } else {
+                feedback.textContent = "Question successfully submitted.";
+                feedback.className = "Message success";
+                addNewFaq(textarea.value.trim());
+                textarea.value = "";
+            }
+               
+        }
+        function addNewFaq(message) {
+            const existingFaq = document.querySelectorAll('.question');
+            const newFaq = document.createElement('div');
+            newFaq.className = 'faq-item';
+            newFaq.textContent = message;
+            existingFaq.appendChild(newFaq);
+        }
+    });
