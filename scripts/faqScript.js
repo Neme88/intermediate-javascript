e4606eb52174ecea8897396e5ef9ea01a12bb7dc
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-07-09 15:03:11 +0200

    added edit faq functionality

--- scripts/faqScript.js
+++ scripts/faqScript.js
@@ -1,59 +1,95 @@
-// function that adds or removes active state on each FAQ as we toggle through.
-const numQuestions = document.getElementById('questions').children;
-function toggleThroughQuestions(event){
-            const fraqQuestions = document.querySelectorAll('.question');
-            fraqQuestions.forEach( function(removeActive) {
-                if(removeActive !== event.currentTarget){
-                    removeActive.classList.remove('active');
-                }
-                
-            });
-    
-             event.currentTarget.classList.toggle('active');   
-};
-const fraqQuestions = document.querySelectorAll('.question');
-fraqQuestions.forEach(function(question) {
-    question.addEventListener('click', toggleThroughQuestions);
-});
-// function that listens for submit event and prevents events to happen by default.
-window.addEventListener('DOMContentLoaded', function() {
-    const newFaq = document.getElementById('newFaq');
-    const textarea = document.getElementById('message');
-    const feedback = document.getElementById('feedback');
+const faqItem = document.querySelectorAll(".faq-item");
+const faqItems = document.getElementById('faq-items');
+const faqEditButton = document.getElementsByClassName('edit');
+const faqHeading = document.getElementsByClassName('faq-heading');
+const form = document.getElementById("faq-form");
+const editForm = document.getElementById("edit-faq-form");
 
-    newFaq.addEventListener('submit', function (event) {
-        event.preventDefault();
-        checkTextarea();
-    
-    });
+let faqs = [];
 
-        // function the check if textarea is empty.
-        function checkTextarea() {
-            console.log('Checking textarea'); 
-            console.log(textarea.value); 
-            if (textarea.value.trim() === '') {
-                feedback.textContent = "Empty textarea.";
-                feedback.className = "Message error";
-            } else {
-                feedback.textContent = "Question successfully submitted.";
-                feedback.className = "Message success";
-                addNewFaq(textarea.value.trim());
-                textarea.value = "";
-            }
-               
+const persistedFaq = JSON.parse(localStorage.getItem("faqs"));
+if(persistedFaq) {
+    faqs = persistedFaq;
+    faqs.forEach(faq => {
+        appendFaq(faq.question, faq.answer)
+    })
+}
+
+for(let x = 0; x < faqEditButton.length; x++) {
+    // Toggle click
+    faqItem[x].addEventListener('click', toggleFaq);
+
+    // Edit click
+    faqEditButton[x].addEventListener('click', function (e) {
+        e.preventDefault()
+        document.getElementById('editQuestion').value = faqHeading[x].innerText
+
+        // add the heading id to the form data-id for easy update
+        editForm.setAttribute('data-id', x);
+    })
+}
+
+// update faq form
+editForm.addEventListener('submit', function (e){
+    e.preventDefault();
+    const question = this.editQuestion.value;
+
+    const index = this.getAttribute('data-id');
+    faqHeading[index].innerText = question;
+
+    // clear input and form
+    editForm.reset();
+    this.removeAttribute('data-id');
+})
+
+// Add Faq
+form.addEventListener('submit', function (e){
+    e.preventDefault()
+    const question = this.question.value;
+    const answer = this.answer.value;
+
+    // faqItems.insertAdjacentHTML("beforeend", faqHtml(question, answer))
+
+    appendFaq(question, answer)
+
+    // persist new faq
+     faqs.push({
+        question: question,
+        answer: answer
+     })
+
+    localStorage.setItem("faqs", JSON.stringify(faqs))
+})
+
+function appendFaq(question, answer) {
+    const newFaqItem = document.createElement('div');
+    newFaqItem.classList.add('faq-item');
+    newFaqItem.innerHTML = `<h3>${question}</h3><p>${answer}</p>`;
+    newFaqItem.addEventListener('click', toggleFaq);
+
+    faqItems.appendChild(newFaqItem);
+}
+
+function faqHtml(question, answer) {
+    return `
+    <div class="faq-item">
+        <h3>${question}</h3>
+        <p>${answer}</p>
+    </div>`
+}
+
+function toggleFaq(element) {
+    document.querySelectorAll(".faq-item").forEach(function(item) {
+        if(item !== element.currentTarget) {
+            item.classList.remove('active')
         }
-        // function that adds new FAQ to the page.
-        function addNewFaq(message) {
-            const faqContainer = document.getElementById('questions');
-            const newFaq = document.createElement('div');
-            const h3 = document.createElement('h3');
-            const p = document.createElement('p');
-            newFaq.className = 'question';
-            //newFaq.textContent = message;
-            h3.textContent = message;
-            p.textContent = "Here I am.";
-            newFaq.appendChild(h3);
-            newFaq.appendChild(p);
-            faqContainer.appendChild(newFaq);
-            newFaq.addEventListener('click', toggleThroughQuestions);        }
-    });
+    })
+    element.currentTarget.classList.toggle('active');
+}
+
+const var1 = "dddd";
+const var2 = "jgmg";
+
+const string = `hhghghghghgh ${var1} ngnnvhffb ${var2}`
+
+// Event bubbling
