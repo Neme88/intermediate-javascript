3cb5611d6e28d5fdc4bcde5aee117c7339f68c33
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-08-09 21:41:33 +0200

    added a traditional function with multiple  parameters,and a traditonal function with default parameter and a traditional function with Rest parameter

--- scripts/allFunctions/traditionalFunctions.js
+++ scripts/allFunctions/traditionalFunctions.js
@@ -1,4 +1,4 @@
-// traditional function with no arguments
+// traditional function with no Parameters
 
 function printSomething() {
     console.log("something...");
@@ -7,7 +7,7 @@ function printSomething() {
 printMe = printSomething();
 console.log(printMe)
 
-// traditional function with one argument
+// traditional function with one Parameters
 
 function squareNumber(num) {
     let result = num * num;
@@ -17,4 +17,33 @@ function squareNumber(num) {
 squareNumber = squareNumber(9);
 console.log(squareNumber)
 
+// traditional function with multiple parameters
+function addThreeNumbers(num1, num2, num3) {
+    let sum = num1 + num2 + num3;
+    return sum;
+}
+
+addthreeNumbers = addThreeNumbers(5, 6, 9)
+
+console.log(addthreeNumbers)
+
+// traditional function with default parameters
+function greetUser(name = "Guest") {
+    console.log("Hello, " + name + "!");
+}
+
+greetUser("John Doe");
+greetUser();
+
+// traditional function with a Rest parameter
+function multiplyNumbers(...nums) {
+    let product = 1;
+    for (let num of nums) {
+        product *= num;
+    }
+    return product;
+}
+
+multiplyNumbers = multiplyNumbers(2, 8, 4, 10);
+console.log(multiplyNumbers)
 
